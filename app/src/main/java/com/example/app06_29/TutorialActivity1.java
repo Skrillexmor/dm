0c36429c54272ad5dad8253ddbdf50a08f2fa003
vivek <vpadia717@rku.ac.in>
2022-07-25 19:57:18 +0530

    commit

--- app/src/main/java/com/example/app06_29/TutorialActivity1.java
+++ app/src/main/java/com/example/app06_29/TutorialActivity1.java
@@ -15,6 +15,7 @@ public class TutorialActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial1);
+        getSupportActionBar().setTitle("Hello RKU");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
