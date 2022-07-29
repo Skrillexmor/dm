e163f24c709c1402a81d4505102d24dc80eda7e0
vivek <vpadia717@rku.ac.in>
2022-07-29 18:18:35 +0530

    autocommit

---
+++ app/src/main/java/com/example/app06_29/TutorialActivity5.java
@@ -0,0 +1,13 @@
+package com.example.app06_29;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorialActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial5);
+    }
+}
